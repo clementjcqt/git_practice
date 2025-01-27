19ff2252394a03aaf633c4cf5ae76d9f77dfbff8
clementjcqt <69344650+clementjcqt@users.noreply.github.com>
2025-01-27 09:10:59 +0100

    First commit

---
+++ message_gen.js
@@ -0,0 +1,24 @@
+let adjs = ["Grand", "Petit", "Funky", "Rapide", "Mystique", "Légendaire", "Brillant", "Étrange", "Marrant", "Rêveur"];
+let nouns = ["Dragon", "Renard", "Tigre", "Étoile", "Ombre", "Pirate", "Mage", "Chevalier", "Phénix", "Vagabond"];
+let suffixes = ["du Désert", "des Bois", "Volant", "Céleste", "de l'Ouest", "Nocturne", "du Chaos", "Rieur", "Flamboyant", "Silencieux"];
+
+const getRandomName = () => {
+    let adj = adjs[Math.floor(Math.random()*adjs.length)];
+    let noun = nouns[Math.floor(Math.random()*nouns.length)];
+    let suffix = suffixes[Math.floor(Math.random()*suffixes.length)]
+    let randomName = adj + ' ' + noun + ' ' + suffix;
+    return randomName;
+}
+
+console.log(getRandomName());
+console.log(getRandomName());
+
+console.log(getRandomName());
+
+console.log(getRandomName());
+
+console.log(getRandomName());
+
+console.log(getRandomName());
+
+
